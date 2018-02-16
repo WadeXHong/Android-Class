de9df6d3ec9fa83901129931c34bb13a03132b5c
wadexhong <wadexhong@gmail.com>
2018-02-17 00:17:55 +0800

    Add selector.xml files to all the buttons in calculator.

--- Week2/src/main/java/com/example/week2/Calculator.java
+++ Week2/src/main/java/com/example/week2/Calculator.java
@@ -2,6 +2,7 @@ package com.example.week2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Calculator extends AppCompatActivity {
 
